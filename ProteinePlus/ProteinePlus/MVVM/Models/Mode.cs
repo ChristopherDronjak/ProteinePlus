569046b2539017522b88ebff3ad9e500cd3d9de9
ChristopherDronjak <yoschmoo@gmail.com>
2023-11-16 16:00:19 +1300

    implemented tictactoe
    because why not

---
+++ ProteinePlus/ProteinePlus/MVVM/Models/Mode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProteinePlus.MVVM.Models
+{
+    public enum Mode
+    {
+        BOT,
+        PLAYER
+    }
+}
